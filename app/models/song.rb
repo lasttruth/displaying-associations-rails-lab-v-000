7c6777c54534eddff9e41f27298b2cac541ed079
Robin Doirin <robindoirin@gmail.com>
2018-07-10 20:09:17 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -7,4 +7,13 @@ class Song < ActiveRecord::Base
   def artist_name
     self.artist.name
   end
+
+  private
+
+
+  def is_title_case
+    if tiitle.split.any?{|w|w[0].upcase != w[0]}
+      errors.add(:name, "Title must be in title case")
+    end
+  end
 end
